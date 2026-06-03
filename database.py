29dd8008e4e6ece62303fc8dfc42b437e9a78924
Michael reques <michaelantoniorequesfernandez@gmail.com>
2026-06-02 22:49:03 -0500

    feat: migrate to postgresql

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-# Creamos un archivo local llamado votacion.db
-SQLALCHEMY_DATABASE_URL = "sqlite:///./votacion.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./votacion.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
